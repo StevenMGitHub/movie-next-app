d68804342bc1fc62cb168ddcd89c91abaaa8d3aa
Steven Munoz <StevenMunoz@innovaGroup.io>
2023-06-23 10:16:24 -0500

    set loading component

--- src/components/Genres.tsx
+++ src/components/Genres.tsx
@@ -10,7 +10,7 @@ const Genres: React.FC<IGenres> = ({ index, name, length}) => {
   return (
     <div className="flex gap-4 text-textColor hover:text-white cursor-pointer">
       <div>{name}</div>
-      <div className="text-textColor">{index + 1 !== length ? "/" : ""}</div> {/* si index no es igual a la longitud '/' si no, mostrar nada porque esta vacio*/}
+      <div className="text-textColor">{index + 1 !== length ? "/" : ""}</div> {/*si index no es igual a la longitud '/' si no, mostrar nada porque esta vacio*/}
     </div>
   );
 };
